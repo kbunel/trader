9ac807f7dbf57aeed0195a3ac088d4831a5e5d85
Samir <samir@tuta.io>
2018-01-23 14:30:01 +0100

    Updated strategy

--- server/strategies/roadTrip.strategy.ts
+++ server/strategies/roadTrip.strategy.ts
@@ -11,12 +11,12 @@ export default class RoadTripStrategy extends Strategy {
 
   public launch(): any {
     return new Promise((resolve): any => {
-      if (this.transactions.allTickers) {
+      if (!this.transactions.allTickers) {
         resolve();
         return;
       }
       const best1HrPercent: CoinMarketCapModel = this.getBest(this.coinMarketCapTools.P_1H);
-      this.logger.log('best 24hr', best1HrPercent);
+      //this.logger.log('best 1hr', best1HrPercent);
 
       this.transactions.getWallet()
       .then((response) => {
@@ -24,6 +24,12 @@ export default class RoadTripStrategy extends Strategy {
         this.logger.log('Wallet =>', this.wallet);
         if (!this.walletContains(best1HrPercent)) {
           // Go buy it !!!!!
+          console.log('**************************');
+          console.log('Change money required');
+          console.log(best1HrPercent);
+          console.log('**************************');
+        } else {
+          console.log('Wallet is OK');
         }
       });
 
@@ -33,11 +39,20 @@ export default class RoadTripStrategy extends Strategy {
 
   private walletContains(coin: CoinMarketCapModel): boolean {
     for (const w of this.wallet) {
-      if (w['asset'] === coin['symbol']) {
+      if (w['asset'] === coin['symbol'] && this.isBestAmountInWallet(w)) {
         return true;
       }
     }
     return false;
+  }
+
+  private isBestAmountInWallet(coin: Wallet): boolean {
+    for (const w of this.wallet) {
+      if (w['asset'] != coin['asset'] && Number(w['free']) > Number(coin['free'])) {
+        return false;
+      }
+    }
+    return true;
   }
 
   private getBest(key: string): CoinMarketCapModel {
