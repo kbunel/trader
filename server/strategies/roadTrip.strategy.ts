74f7bccb14db3f136e0b75e3743307ac1e5d5825
kbunel <kevinbunel@gmail.com>
2018-02-26 21:56:55 +0100

    fix

--- server/strategies/roadTrip.strategy.ts
+++ server/strategies/roadTrip.strategy.ts
@@ -59,7 +59,7 @@ export default class RoadTripStrategy extends Strategy {
               this.getBestFromCoinMarketCap(this.coinMarketCapTools.P_1H);
               this.getBestFromBinance();
               this.orderManager.sellEverything(best.symbol);
-              this.orderManager.buyIfPossible(best);
+              this.orderManager.buyIfPossible(best.symbol);
             } else if (this.orderManager.getCurrentOrders(best.symbol).length) {
               this.logger.log('Best ticker found in current order, let\'s check if it s still available');
 
